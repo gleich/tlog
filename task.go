0320f9f5fa34cd9a8a96e95c1ba05293c5f182cc
Matt Gleich <git@mattglei.ch>
2026-03-12 11:05:09 -0400

    feat: start method on a given task

--- task.go
+++ task.go
@@ -1,7 +1,13 @@
 package tlog
 
+import "time"
+
 type Task string
 
 func (t Task) Extend(task string) Task {
 	return Task(string(t) + "-" + task)
+}
+
+func (t Task) Start() (Task, time.Time) {
+	return t, time.Now()
 }
